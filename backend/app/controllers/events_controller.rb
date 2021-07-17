412e927aedd979128c064676ca0fa2dda94dce61
kei-600th <kei19941005@gmail.com>
2021-07-17 18:00:19 +0900

    1-6 イベント詳細取得APIを作成する

--- backend/app/controllers/events_controller.rb
+++ backend/app/controllers/events_controller.rb
@@ -4,5 +4,10 @@ class EventsController < ApplicationController
     # イベントデータの一覧を返す
     render json: Event.all
   end
-  
+
+  def show
+    # 指定したidのイベントデータを返す
+    render json: Event.find(params[:id])
+  end
+
 end
